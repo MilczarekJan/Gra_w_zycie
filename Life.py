c92997b64a75c1b6551fd5587f8b85cc681a6b36
Jan <01169598@pw.edu.pl>
2024-05-23 17:53:27 +0100

    Klasa LifeCell

--- Life.py
+++ Life.py
@@ -4,6 +4,14 @@ from pygame.locals import *
 BLUE = (173, 216, 230)
 RED = (255, 0, 0)
 
+
+class LifeCell:
+    def __init__(self, cords, center_cords, id, color) -> None:
+        self.CellCords = cords
+        self.CenterCords = center_cords
+        self.Id = id
+        self.Color = color
+
 def select_mode(size):
     rectangle_amount = size*size
     left = 1
@@ -19,10 +27,12 @@ def select_mode(size):
             if rect_num % 10 == 0 and rect_num != 0:
                 top = top + distance
             left = left + (rect_num%10)*distance
-            curr_rect = (left, top, width, height)
+            x_center = left + (width/2)
+            y_center = top + (height/2)
+            curr_rect = LifeCell((left, top, width, height), (x_center, y_center), rect_num, BLUE)
             rectangles.append(curr_rect)
     elif size == 100:
-        display_mode = (1000, 1000) #10x10, przerwa 2, początek i koniec 1
+        display_mode = (1000, 1000)
         width = 8
         height = 8
         distance = width + 2
@@ -32,7 +42,9 @@ def select_mode(size):
             if rect_num % 100 == 0 and rect_num != 0:
                 top = top + distance
             left = left + (rect_num%100)*distance
-            curr_rect = (left, top, width, height)
+            x_center = left + (width/2)
+            y_center = top + (height/2)
+            curr_rect = LifeCell((left, top, width, height), (x_center, y_center), rect_num, BLUE)
             rectangles.append(curr_rect)
     return display_mode, rectangles
             
@@ -42,12 +54,11 @@ display_mode, rectangles = select_mode(int(sys.argv[1]))
 screen = pygame.display.set_mode(display_mode)
 screen.fill((255, 255, 255))
 for rect in rectangles:
-    pygame.draw.rect(screen, BLUE, rect)
+    pygame.draw.rect(screen, rect.Color, rect.CellCords)
 pygame.display.flip()
 
 # Variable to keep our game loop running
 gameOn = True
-
 while gameOn:
     # for loop through the event queue
     for event in pygame.event.get():        
